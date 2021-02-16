e6563a5f117b6cade3cf1601b687925dcce75be5
Artur Molla <mollaartur13@gmail.com>
2021-02-16 15:36:06 +0100

    Add comments on Test file

--- Coin/Test.java
+++ Coin/Test.java
@@ -8,6 +8,12 @@ public class Test {
 		System.out.println("flip result "+c1.flip());
 		System.out.println("flip result "+c1.flip());
 		System.out.println("flip result "+c1.flip());
+		
+//		Duke qene se metoden totalNumbersOfFlipForThisCoin do e therrasim per objekte te
+//		ndryshme duhet te kemi nje variabel qe mban numerimin jo static pasi vlera e tij do ndryshoje 
+//		me objekte te ndryshme dhe rrjedhimisht metoda nuk do jete statike dhe kemi mundsi 
+//		ta therrasim per cdo objekt qe krijojm
+//		
 		System.out.println("total flip for "+c1+" = "+c1.totalNumbersOfFlipForThisCoin());
 		
 		Coin c2 = new Coin();
@@ -16,6 +22,11 @@ public class Test {
 		System.out.println("flip result "+c2.flip());
 		System.out.println("flip result "+c2.flip());
 		System.out.println("total flip for "+c2+" = "+c2.totalNumbersOfFlipForThisCoin());
+	
+//		Per te aksesuar metoden totalNumberOfFlipsForAllCoins direkt nga klasa pa e therritur 
+//		me objekt duhet ta krijojm si metode statike dhe kemi perdorur nje variabel static per te 
+//		mbajtur numrimin e thirrjes se flip gjate ekzekutimit.
+//		variablat static i aksesojme vetem ne metoda statike.
 		Coin.totalNumberOfFlipsForAllCoins();
 	}
 	
